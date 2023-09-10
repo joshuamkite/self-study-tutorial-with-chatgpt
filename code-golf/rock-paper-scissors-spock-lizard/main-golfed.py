bedc8e0a53fd62113c762f8d779f7b9c9ec68522
Joshua Kite <joshuamkite@gmail.com>
2023-09-10 12:02:59 +0100

    golfed spock

---
+++ code-golf/rock-paper-scissors-spock-lizard/main-golfed.py
@@ -0,0 +1,29 @@
+import sys
+
+emoji_outcomes = {
+    "✂📄": "cuts",
+    "💎🦎": "crushes",
+    "🖖✂": "smashes",
+    "🦎📄": "eats",
+    "🖖💎": "vaporizes",
+    "📄💎": "covers",
+    "💎✂": "crushes",
+    "📄🖖": "disproves",
+    "🦎🖖": "poisons",
+    "✂🦎":  "decapitates"
+}
+
+# Accessing arguments
+for arg in sys.argv[1:]:
+    if arg[0] == arg[1]:
+        print("Tie")
+        continue
+    outcome = emoji_outcomes.get(arg)
+    reverse = False
+    if outcome is None:
+        outcome = emoji_outcomes.get(arg[1] + arg[0])
+        reverse = True
+    if reverse:
+        print(arg[1], outcome, arg[0])
+    else:
+        print(arg[0], outcome, arg[1])
