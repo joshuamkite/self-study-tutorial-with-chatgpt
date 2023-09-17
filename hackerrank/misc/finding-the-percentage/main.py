cf3d9e98bfac8e2b762c83ef206b459b9042dee0
Joshua Kite <joshuamkite@gmail.com>
2023-09-17 21:11:24 +0100

    forgotten saves

--- hackerrank/misc/finding-the-percentage/main.py
+++ hackerrank/misc/finding-the-percentage/main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    # write your code here:
     score = student_marks[query_name]
     mean_score = sum(score)/(len(score))
     print(f"{mean_score:.2f}")
